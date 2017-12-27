2de657536f54cb25602e87dc337809df19c8b82f
croz0034 <32398078+croz0034@users.noreply.github.com>
2017-12-27 02:27:23 -0500

    Trying to fix github issues

--- JS/main.js
+++ JS/main.js
@@ -1,5 +1,7 @@
 const KEY = 'CROZ0034';
 let contacts = [];
+let choppingBlock;
+let isthisanedit;
 
 const init = function(){
     if(!localStorage.getItem(KEY)){
@@ -16,22 +18,51 @@ const init = function(){
 }
 
 const updateList = function(){
+    isthisanedit = 0;
+    
+    let alphabetical;
+    let newOrder = ["placeholder"];
+    for ( i of contacts){
+        newOrder.push(i.fullname);
+        console.log(newOrder);
+    };
+    newOrder.sort();
+    console.log(newOrder);
+    let oldorder = contacts;
+    contacts = [];
+    for( x=0; x <= newOrder.length; x++){
+        for (y of oldorder){
+            filter = newOrder[x];
+            filteritem = JSON.stringify(y)
+            if(filteritem.includes(filter)){contacts.push(JSON.parse(filteritem))}
+        }
+    }
+console.log(contacts)
+
     let ul = document.querySelector('ul.contacts');
     ul.innerHTML="";
     let df = new DocumentFragment();
-    contacts.forEach((contact)=>{
-        df.appendChild( createItem(contact) );
-    })
+    for (q of contacts){
+        df.appendChild( createItem(q) );
+    } 
     ul.appendChild(df);
+    
+    localStorage.setItem(KEY, JSON.stringify(contacts));
 }
 
 const createItem = function(contact){
     let li = document.createElement('li');
-    li.className = 'contact';    
+    li.className = 'contact';
     let span = document.createElement('span');
     span.className = 'delete';
     span.setAttribute("data-key", contact.email);
     span.addEventListener('click', removeContact);
+    li.appendChild(span);
+    span = document.createElement('span');
+    span.className = 'edit';
+    span.setAttribute("data-key", contact.email);
+    span.info = contact;
+    span.addEventListener('click', edit);
     li.appendChild(span);
     let h3 = document.createElement('h3');
     h3.textContent = contact.fullname;
@@ -62,9 +93,44 @@ const hideForm = function(ev){
     document.querySelector('.contactform').style.display = 'none';
     document.querySelector('.overlay').style.display = 'none';
     document.querySelector('.contactform form').reset();
+    let fullname = document.getElementById('input-name');
+    fullname.textContent = "";
+    let email = document.getElementById('input-email');
+    email.textContent = "";
+    let phone = document.getElementById('input-phone');
+    phone.textContent = "";
+    isthisanedit = 0;
 }
 
 const addContact = function(ev){
+    console.log(isthisanedit);
+    if(isthisanedit == 0){ console.log('not an edit');
+newContact(ev)}
+    
+    else{console.log('this was an edit'); editContact(ev)}}
+
+function editContact(ev){
+    console.log('edit start');
+
+    ev.preventDefault()
+        
+    let email = choppingBlock; console.log(email)
+    
+    console.log(email)
+    contacts = contacts.filter(
+        (contact)=>{
+        console.log(contact.email, email)
+        return!(contact.email == email);
+    });
+    console.log(contacts)
+    localStorage.setItem(KEY, JSON.stringify(contacts));
+    
+    newContact(ev);
+    updateList();
+    
+}
+
+function newContact(ev){    
     ev.preventDefault;
     let fullname = document.getElementById('input-name').value.trim();
     let email = document.getElementById('input-email').value.trim();
@@ -76,14 +142,17 @@ const addContact = function(ev){
         localStorage.setItem(KEY, JSON.stringify(contacts));
         document.querySelector('.contactform form').reset();
         hideForm(new MouseEvent('click'));
-        updateList();}
+        updateList();
+    }
     else{ alert('Form not filled in')}
+    
     }
 
 
 const removeContact = function(ev){
     ev.preventDefault()
     let email = ev.target.getAttribute('data-key');
+    
     console.log(email)
     contacts = contacts.filter(
         (contact)=>{
@@ -94,7 +163,21 @@ const removeContact = function(ev){
     localStorage.setItem(KEY, JSON.stringify(contacts));
     updateList();
 }
-
+function edit(ev){
+    let editTarget = this.info;
+    isthisanedit = 1;
+    ev.preventDefault();
+    choppingBlock = ev.target.getAttribute('data-key');
+    console.log(choppingBlock);
+    console.log(editTarget);
+    let fullname = document.getElementById('input-name');
+    fullname.value = editTarget.fullname;
+    let email = document.getElementById('input-email');
+    email.value =  editTarget.email;
+    let phone = document.getElementById('input-phone');
+    phone.value = editTarget.phone
+    showForm(ev);
+}
 
 
 
